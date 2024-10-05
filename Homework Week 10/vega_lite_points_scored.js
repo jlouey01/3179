4911567c9b8d7f9dfc71e80e57993bc1eb3abaa0
jlouey01 <jonlouey2001@gmail.com>
2024-10-05 21:26:37 +1000

    Week 10 Homework Completed

---
+++ Homework Week 10/vega_lite_points_scored.js
@@ -0,0 +1,4 @@
+var vg_2 = "points_scored.json";
+vegaEmbed("#points", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
